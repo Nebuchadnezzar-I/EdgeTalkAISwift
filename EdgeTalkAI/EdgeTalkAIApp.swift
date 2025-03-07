a3cd94e5c867b889efee29175859bbe8f19128c1
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-03-07 19:41:11 +0100

    Initial Commit

---
+++ EdgeTalkAI/EdgeTalkAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeTalkAIApp.swift
+//  EdgeTalkAI
+//
+//  Created by Michal Ukropec on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct EdgeTalkAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
